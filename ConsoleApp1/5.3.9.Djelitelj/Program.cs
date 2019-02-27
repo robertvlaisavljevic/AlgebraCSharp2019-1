70415ff20f7a17157dd903890cc246ca82ec5f2f
robert vlaisavljevic <korisnik92@gmail.com>
2019-02-27 18:24:27 +0100

    dodan zadatak

---
+++ ConsoleApp1/5.3.9.Djelitelj/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._3._9.Djelitelj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi broj ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("djeljivi brojevi sa {0} su:", a);
+
+            for (int i = 1; i <= a; i++)
+            {
+              if (a % i == 0)
+                {
+                    Console.Write("{0}, ", i);
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
