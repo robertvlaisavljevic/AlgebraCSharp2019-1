3fdb708ccd4f04e37c1badd814f9bb6dec5079f1
robert vlaisavljevic <korisnik92@gmail.com>
2019-02-28 15:44:34 +0100

    zadatak umnozak i kvocijent

---
+++ ConsoleApp1/umnozak_kvocijent2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace umnozak_kvocijent2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+            
+            Console.WriteLine("Unesite prvi broj: ");
+            a=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesite drugi broj: ");
+            b=int.Parse(Console.ReadLine());
+
+            //uvijek prvo decimal pa int
+            Console.WriteLine("Umnozak ovih brojeva je: {0}", a*b);
+            Console.WriteLine("Kvocijent ovih brojeva je: {0}", (decimal)a / (decimal)b);
+
+            Console.ReadKey();
+
+        }
+    }
+}
