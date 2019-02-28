c45e87bf69a7b973fc298678de9428b79300b4af
robert vlaisavljevic <korisnik92@gmail.com>
2019-02-28 18:07:24 +0100

    x-znamenkasti broj zadatak 5.2.4

---
+++ ConsoleApp1/x-znamenkasti broj2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace x_znamenkasti_broj2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesi broj: ");
+            int a=int.Parse(Console.ReadLine());
+
+            if (a < 0) a = -a;
+            if (a>0 && a<10)
+            
+            {
+                Console.WriteLine("broj je jednoznamenkast");
+            }
+
+            else if (a<100)
+
+            {
+                Console.WriteLine("broj je dvoznamenkast");
+            }
+
+            else if (a<1000)
+
+            {
+                Console.WriteLine("broj je troznamenkast");
+            }
+
+            else if (a>999)
+
+            {
+                Console.WriteLine("broj je viseznamenkast");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
