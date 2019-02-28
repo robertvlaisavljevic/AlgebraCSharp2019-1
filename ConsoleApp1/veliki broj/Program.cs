c3d0f33089c96e5a357d8ffc2ace440da42afe61
robert vlaisavljevic <korisnik92@gmail.com>
2019-02-28 17:38:14 +0100

    veliki broj

---
+++ ConsoleApp1/veliki broj/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace veliki_broj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            Console.Write("Unesi broj: ");
+            a=int.Parse(Console.ReadLine());
+
+            if (a>99)
+            {
+                Console.WriteLine("Broj je VELIK !");
+            }
+            else
+            {
+                Console.WriteLine("Broj NIJE velik" );
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
