54379ae96cbf7edab3a7c3f700d070c24cf1c5a1
robert vlaisavljevic <korisnik92@gmail.com>
2019-02-28 15:30:08 +0100

    moze svaki za sebe, a mogu u istom redu

--- ConsoleApp1/5.1.1_zbroj_i_razlika(1)/Program.cs
+++ ConsoleApp1/5.1.1_zbroj_i_razlika(1)/Program.cs
@@ -12,14 +12,19 @@ namespace _5._1._1_zbroj_i_razlika_1_
         {
             
             int a = 0, b = 0;
-
+            
             Console.WriteLine("Unesite prvi broj: ");
             a = int.Parse(Console.ReadLine());
             Console.WriteLine("Unesite drugi broj: ");
             b = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Zbroj {0}: ", a + b);
-            Console.WriteLine("Razlika {0}: ", a - b);
+            //svaki posebno
+            Console.WriteLine("Zbroj je: {0} ", a + b);
+            Console.WriteLine("Razlika je: {0}: ", a - b);
+
+
+            //svaki za sebe
+            Console.WriteLine("Zbroj je: {0}, Razlika je: {1}  ", a + b, a - b);
 
             Console.ReadKey();
         }
