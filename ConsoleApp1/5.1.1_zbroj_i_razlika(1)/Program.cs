c0bb1262ce8d37dff2353f44a1938e6ce952db21
robert vlaisavljevic <korisnik92@gmail.com>
2019-02-28 13:32:28 +0100

    int.parse za brojeve

---
+++ ConsoleApp1/5.1.1_zbroj_i_razlika(1)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._1._1_zbroj_i_razlika_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int a = 0, b = 0;
+
+            Console.WriteLine("Unesite prvi broj: ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Unesite drugi broj: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Zbroj {0}: ", a + b);
+            Console.WriteLine("Razlika {0}: ", a - b);
+
+            Console.ReadKey();
+        }
+    }
+}
