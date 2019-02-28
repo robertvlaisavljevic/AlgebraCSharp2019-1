a3a0c1dd54060226fa326c97a90abfe2c52d8240
robert vlaisavljevic <korisnik92@gmail.com>
2019-02-28 13:07:26 +0100

    zasto ne moze ovako?

---
+++ ConsoleApp1/vreliste_lediste_vode2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vreliste_lediste_vode2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //proba
+            Console.WriteLine("vreliste vode je 100 stupnjeva");
+            Console.WriteLine("lediste vode je 0 stupnjeva");
+            Console.ReadKey();
+        }
+    }
+}
