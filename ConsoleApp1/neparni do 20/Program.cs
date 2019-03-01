e511eec73b9d5392dfc9c82cec78744a7aed4d07
robert vlaisavljevic <korisnik92@gmail.com>
2019-03-01 11:41:19 +0100

    ne parni od 3 do 20

---
+++ ConsoleApp1/neparni do 20/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace neparni_do_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Neparni brojevi od 3 do 20 su: ");
+            for (int i = 3; i <= 19; i+=2)
+            {
+              Console.Write("{0 }  ", i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
