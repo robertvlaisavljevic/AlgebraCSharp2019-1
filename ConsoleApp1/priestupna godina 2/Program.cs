f21476507bbfc588fae4d9682b36f3141649303a
robert vlaisavljevic <korisnik92@gmail.com>
2019-03-01 11:06:54 +0100

    prijestupna godina

---
+++ ConsoleApp1/priestupna godina 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace priestupna_godina_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Upisite godinu: ");
+            int a=int.Parse(Console.ReadLine());
+
+            if ((a%4==0 && (!(a%100==0)))||a%400==0 )
+            {
+                Console.WriteLine("Godina je prijestupna. ");
+            }
+
+            else 
+            {
+                Console.WriteLine("Godina nije prijestupna. ");
+            }
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
