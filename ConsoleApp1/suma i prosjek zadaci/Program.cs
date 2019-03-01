a3d37d2262c4436c316b79a6c3e4152e66bfae02
robert vlaisavljevic <korisnik92@gmail.com>
2019-03-01 18:13:32 +0100

    nove promjene

---
+++ ConsoleApp1/suma i prosjek zadaci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace suma_i_prosjek_zadaci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma=0;
+            float prosjek;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.Write("Unesite {0 } broj:", i);
+                suma+=int.Parse(Console.ReadLine());
+            }
+
+            prosjek=(float)suma / 5;
+
+            Console.WriteLine("suma= {0}\nProsjek = {1}", suma, prosjek);
+            Console.ReadKey();
+
+        }
+    }
+}
