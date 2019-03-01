7843b4126c48ce6b02e0c9af5af4eaf4eb47ea65
robert vlaisavljevic <korisnik92@gmail.com>
2019-03-01 17:35:05 +0100

    suma i prosjek

---
+++ ConsoleApp1/suma i prosjek/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace suma_i_prosjek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=0, b=0, c=0, d=0, e=0;
+
+
+            Console.Write("Unesite prvi broj: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Unesite drugi broj: ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Unesite treci broj: ");
+            c = int.Parse(Console.ReadLine());
+            Console.Write("Unesite cetvrti broj: ");
+            d = int.Parse(Console.ReadLine());
+            Console.Write("Unesite peti broj: ");
+            e = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nSuma ovih brojeva je: {0}", a+b+c+d+e);
+            Console.WriteLine("\nProsjek ovih brojeva je {0}", (a + b + c + d + e) / 5);
+
+            Console.ReadKey();
+
+        }
+        
+
+    }
+    
+    
+
+}
