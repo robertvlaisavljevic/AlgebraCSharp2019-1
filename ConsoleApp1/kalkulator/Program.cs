30139ee91ac80823b65560b82c222c7e1fb39c9d
robert vlaisavljevic <korisnik92@gmail.com>
2019-03-02 19:59:54 +0100

    kalkulator

--- ConsoleApp1/kalkulator/Program.cs
+++ ConsoleApp1/kalkulator/Program.cs
@@ -12,46 +12,51 @@ namespace kalkulator
         {
             float a=0, b = 0;
             string operacija = "";
+            string odgovor = "D";
 
-            Console.WriteLine("Unesite prvi broj: ");
-            a = float.Parse(Console.ReadLine());
+            while(odgovor =="D")
+            { 
+                Console.WriteLine("Unesite prvi broj: ");
+                a = float.Parse(Console.ReadLine());
 
-            Console.WriteLine("Unesite drugi broj: ");
-            b = float.Parse(Console.ReadLine());
+                Console.WriteLine("Unesite drugi broj: ");
+                b = float.Parse(Console.ReadLine());
 
-            Console.WriteLine("Unesite racunsku operaciju (+,-,* ili /): ");
-            operacija = Console.ReadLine();
+                Console.WriteLine("Unesite racunsku operaciju (+,-,* ili /): ");
+                operacija = Console.ReadLine();
 
 
-            switch (operacija)
-            {
+                switch (operacija)
+                {
                 case "+":
 
-            Console.WriteLine("Zbroj ovih brojeva je {0}:", a+b);
+                Console.WriteLine("Zbroj ovih brojeva je {0}:", a+b);
                     break;
 
                 case "-":
 
-             Console.WriteLine("Razlika ovih brojeva je {0}:", a - b);
+                Console.WriteLine("Razlika ovih brojeva je {0}:", a - b);
                     break;
 
                 case "*":
 
-             Console.WriteLine("Umnozak ovih brojeva je {0}:", a * b);
+                Console.WriteLine("Umnozak ovih brojeva je {0}:", a * b);
                     break;
 
                 case "/":
 
-             Console.WriteLine("Kvocijent ovih brojeva je {0}:", (decimal)a / (decimal)b);
+                Console.WriteLine("Kvocijent ovih brojeva je {0}:", (decimal)a / (decimal)b);
                     break;
                 default:
                     Console.WriteLine("Nepoznata operacija!");
                     break;
+                }
+                Console.WriteLine("Zelite li racunati ponovo:(D/N) ?");
+                odgovor = Console.ReadLine();
+
             }
             
-
-
-            Console.WriteLine("Pritisnite neku tipku za kraj...");
+            
             Console.ReadKey();
         }
         
