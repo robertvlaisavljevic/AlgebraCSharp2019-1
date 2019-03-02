c111648f6ff6e1f359f89e97e8170cd9e9c9f2ac
robert vlaisavljevic <korisnik92@gmail.com>
2019-03-02 13:19:02 +0100

    5.3.14 Neparni brojevi

---
+++ ConsoleApp1/5.3.14 Neparni brojevi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._3._14_Neparni_brojevi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Neparni brojevi od 1 do 10 su: ");
+            for (int i = 1; i <= 10; i+=2)
+            {
+                Console.Write("\n{0} ", i);
+            }
+            
+            Console.ReadKey();
+
+
+        }
+    }
+}
