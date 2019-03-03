c37fa74d8db0b49ede513b9a7c22de3a9dc4cb4a
robert vlaisavljevic <korisnik92@gmail.com>
2019-03-03 19:02:47 +0100

    dodano ponavljanje

---
+++ ConsoleApp1/KalkulatorP/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalkulatorP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                float a = 0, b = 0;
+                string operacija = "";
+                string odgovor = "D";
+
+                while (odgovor == "D" || odgovor == "d")
+                {
+
+                    Console.Write("upisite 1.broj: ");
+                    a = float.Parse(Console.ReadLine());
+                    Console.Write("Upisite 2. broj: ");
+                    b = float.Parse(Console.ReadLine());
+
+
+                    Console.Write("Upisite racunsku operaciju; +,-,*,/ : ");
+                    operacija = Console.ReadLine();
+
+                    switch (operacija)
+                    {
+
+
+                        case "+":
+                            Console.WriteLine("Zbroj iznosi: {0}", a + b);
+                            break;
+
+                        case "-":
+                            Console.WriteLine("Razlika iznosi: {0}", a - b);
+                            break;
+
+                        case "*":
+                            Console.WriteLine("Umnozak iznosi: {0}", a * b);
+                            break;
+
+                        case "/":
+                            Console.WriteLine("Kvocijent iznosi: {0}", a / b);
+                            break;
+
+                        default:
+                            Console.WriteLine("Nepoznata racunska operacija!");
+                            break;
+                    }
+                    Console.WriteLine("Zelite li racunati ponovo:(D/N) ?");
+                    odgovor = Console.ReadLine();
+
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.Write("GRESKA!\nMorate upisati broj", ex.ToString());
+                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
