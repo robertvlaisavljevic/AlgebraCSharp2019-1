0a32211a0f483d61642873a4dfe623b816a23d97
robert vlaisavljevic <korisnik92@gmail.com>
2019-03-04 14:19:36 +0100

    djeliteljP

---
+++ ConsoleApp1/DjeliteljP/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DjeliteljP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 0;
+                string odgovor = "";
+
+                
+                Console.Write("Upisite broj: ");
+                a = int.Parse(Console.ReadLine());
+                Console.Write("Svi brojevi djeljivi sa {0} su: ", a);
+               
+                    for (int i = 1; i <= a; i++)
+                    {
+                    if (a % i == 0)
+                    {
+                        Console.Write("{0} ", i);
+                    }
+                        
+                    }
+                    
+                
+            }
+            catch (Exception broj)
+            {
+                Console.Write("GRESKA!", broj);
+            }
+            Console.ReadKey();
+        
+        }
+    }
+}
