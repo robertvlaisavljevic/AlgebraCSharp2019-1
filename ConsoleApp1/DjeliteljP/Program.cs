6504b4bfb474c44584d5432bf1729160a2e70f56
robert vlaisavljevic <korisnik92@gmail.com>
2019-03-04 19:50:45 +0100

    6.1.2 max nizovi

--- ConsoleApp1/DjeliteljP/Program.cs
+++ ConsoleApp1/DjeliteljP/Program.cs
@@ -14,22 +14,26 @@ namespace DjeliteljP
             {
                 int a = 0;
                 string odgovor = "";
-
                 
-                Console.Write("Upisite broj: ");
-                a = int.Parse(Console.ReadLine());
-                Console.Write("Svi brojevi djeljivi sa {0} su: ", a);
+
+                    Console.Write("Upisite broj: ");
+                    a = int.Parse(Console.ReadLine());
+                    Console.Write("Svi brojevi djeljivi sa {0} su: ", a);
                
                     for (int i = 1; i <= a; i++)
                     {
-                    if (a % i == 0)
-                    {
-                        Console.Write("{0} ", i);
-                    }
+                        if (a % i == 0)
+                            {
+                             Console.Write("{0} ", i);
+                            }
                         
                     }
-                    
-                
+                while (odgovor == "D" || odgovor == "d" || odgovor == "da" || odgovor == "DA")
+                {
+                    Console.WriteLine("\nZelite li upisati drugi broj: (D/N)", odgovor);
+                    odgovor = Console.ReadLine();
+
+                }
             }
             catch (Exception broj)
             {
