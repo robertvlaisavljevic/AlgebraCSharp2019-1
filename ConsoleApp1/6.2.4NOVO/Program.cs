04051bc4d8e10175fb098987c06c54cf74caa8ea
robertvlaisavljevic <korisnik92@gmail.com>
2019-03-05 18:15:36 +0100

    prosti

---
+++ ConsoleApp1/6.2.4NOVO/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._2._4NOVO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string a =Console.ReadLine();
+            int ua = int.Parse(Console.ReadLine());
+            a.Reverse();
+            Console.Write("original {0}, naopako {1} ", ua, new string(a.Reverse().ToArray()));
+            Console.ReadKey();
+        }
+        
+    }
+}
