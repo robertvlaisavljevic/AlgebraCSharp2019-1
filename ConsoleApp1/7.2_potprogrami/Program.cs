91cb14cb1dfc98f1c0487cc713603e74ac929e8b
robertvlaisavljevic <korisnik92@gmail.com>
2019-03-05 18:48:23 +0100

    novo

---
+++ ConsoleApp1/7.2_potprogrami/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._2_potprogrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Program.HelloWorld();
+            Console.WriteLine(HelloWorld("ola"));
+            Console.ReadKey();
+        }
+        static void HelloWorld()
+        {
+            Console.WriteLine("Helloooo");
+        }
+        static String HelloWorld(String poz)
+        {
+            return poz + " I tebi putnice";
+        }
+    }
+}
