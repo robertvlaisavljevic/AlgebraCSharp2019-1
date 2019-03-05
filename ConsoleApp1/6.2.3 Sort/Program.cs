dacf695ab0984799fec6984de88b8269af94f263
robertvlaisavljevic <korisnik92@gmail.com>
2019-03-05 17:37:32 +0100

    novo nizovi

---
+++ ConsoleApp1/6.2.3 Sort/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._2._3_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arr  = new ArrayList();
+            
+            int a = -1;
+            Console.WriteLine("Unesite prirodne brojeve, za kraj unesite 0");
+            
+
+            while (a != 0)
+            {
+                a = int.Parse(Console.ReadLine());
+                arr.Add(a);
+            }
+            
+            arr.Sort();
+
+            foreach (int item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
