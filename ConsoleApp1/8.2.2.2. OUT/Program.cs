9220bc404ee64071143db25bf9c34087244cced5
robertvlaisavljevic <korisnik92@gmail.com>
2019-03-12 16:13:44 +0100

    out

---
+++ ConsoleApp1/8.2.2.2. OUT/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._2._2._2.OUT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Inicijaliziraj(out a);
+            Console.WriteLine("a={0}", a);
+            Console.ReadKey();
+        }
+        private static void Inicijaliziraj (out int a)
+        {
+            a = 1;
+        }
+    }
+}
