99ebb4551f1bd0711505e066d4b16f1e3da725b4
robert vlaisavljevic <korisnik92@gmail.com>
2019-03-13 17:04:17 +0100

    dogadjaji

---
+++ ConsoleApp1/8.3.1_nasljedjivanje/Ispit.cs
@@ -0,0 +1,12 @@
+﻿namespace _8._3._1_nasljedjivanje
+{
+    public class Ispit
+    {
+        private string v;
+
+        public Ispit(string v)
+        {
+            this.v = v;
+        }
+    }
+}
