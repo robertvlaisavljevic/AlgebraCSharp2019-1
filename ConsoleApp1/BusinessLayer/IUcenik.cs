77ac1998681f9e817fc58e6f297eec3001710e84
robert vlaisavljevic <korisnik92@gmail.com>
2019-03-13 17:05:02 +0100

    dada

---
+++ ConsoleApp1/BusinessLayer/IUcenik.cs
@@ -0,0 +1,10 @@
+﻿namespace _8._3._1_nasljedjivanje
+{
+    internal interface IUcenik
+    {
+        double Prosjek();
+        string ToString();
+        void Prijaviispit(Ispit ispit);
+        
+    }
+}
