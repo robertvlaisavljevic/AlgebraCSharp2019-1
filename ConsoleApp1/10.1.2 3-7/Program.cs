c83b01586cc85e9d139767d0d9bd79bc98c55273
robert vlaisavljevic <korisnik92@gmail.com>
2019-03-14 18:47:26 +0100

    dfsao

---
+++ ConsoleApp1/10.1.2 3-7/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._1._2_3_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("3or7uNoviRed.txt");
+            Console.WriteLine("3or7saZarezima.txt");
+            
+
+            FileStream fs1 = new FileStream("3or7uNoviRed.txt", FileMode.Create);
+            StreamWriter sw1 = new StreamWriter(fs1);
+            FileStream fs2 = new FileStream("3or7saZarezima.txt", FileMode.Create);
+            StreamWriter sw2 = new StreamWriter(fs2);
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i%3==0 || i%7==0)
+                {
+                    sw1.WriteLine(i);
+                    sw2.Write("{0},", i);
+                }
+                
+            }
+
+            sw1.Flush();
+            sw1.Close();
+            fs1.Close();
+            sw2.Flush();
+            sw2.Close();
+            fs2.Close();
+
+
+            fs1 = new FileStream("3or7uNoviRed.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(fs1);
+            string s = sr.ReadToEnd();
+            Console.WriteLine(s);
+            sr.Close();
+            fs1.Close();
+
+            fs1 = new FileStream("3or7saZarezima.txt", FileMode.Open);
+            sr = new StreamReader(fs1);
+            s = sr.ReadToEnd();
+            Console.WriteLine(s);
+            sr.Close();
+            fs1.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
